39c72527b2638170a9268a10a13f587bbf3b0610
AndiCucka <124384763+AndiCucka@users.noreply.github.com>
2024-04-30 09:30:03 -0400

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,31 @@
+/*
+* This program calculates the factorial of a number.
+*
+* @author  Andi Cucka
+* @version 1.0
+* @since   2024-04-24
+*/
+
+import { createPrompt } from 'bun-promptx'
+
+function factorial(someInt: number): number {
+  /*
+  * This function calculates the factorial using recursion.
+  */
+  if (someInt < 0) {
+    return -1
+  } else {
+    if (someInt <= 1) {
+      return 1
+    } else {
+      return (someInt * factorial(someInt - 1))
+    }
+  }
+}
+
+const userInput = createPrompt(`Entered = `)
+const someInt = parseInt(userInput.value)
+let factorialOfInt = factorial(someInt)
+console.log(`Returned = ${factorialOfInt}`)
+
+console.log(`\nDone.`)
